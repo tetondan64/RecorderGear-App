fd5830e46fef6ee26163f7160dfac548ed8ebf47
tetondan64 <dan@inventivbrands.com>
2025-08-09 17:29:55 -0600

    style: tweak footer padding

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,8 +32,8 @@ export default function TabLayout() {
             height: 90 + insets.bottom,
             paddingBottom: 20 + insets.bottom,
             paddingTop: 15,
-            paddingLeft: 30,
-            paddingRight: 10,
+            paddingLeft: 35,
+            paddingRight: 0,
           },
         }}>
         <Tabs.Screen
